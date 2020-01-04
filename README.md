5205fbae6690f6fe7733e3ded57eb7c5c377e45e
ShifengChen <shifengchen10@gmail.com>
2020-01-04 22:27:02 +0800

    add a database

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # TODO SERVER
 
 node, graphql, prisma
+
+## how to run
+
+### To start Prisma and launch the connected database
+
+```bash
+cd todo-demo-server
+docker-compose up -d
+```
+
+### Deploy the Prisma datamodel
+
+```bash
+prisma deploy
+```
+
+### Start
+
+```bash
+npm run start
+```
--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
-const { GraphQLServer } = require('graphql-yoga')
-const moment = require('moment')
+const { GraphQLServer } = require("graphql-yoga");
+const moment = require("moment");
+const { prisma } = require("../generated/prisma-client");
 
-let list =[{
-  id: 'id-0',
-  info: 'learn graphql',
-  deadline: '2020-01-05',
-  level: 3,
-  done: false,
-  createAt: '2020-01-03'
-}]
+async function main() {
+  // Create a new todo
+  // const newTodo = await prisma.createTodo({
+  //   info: "learn graphql",
+  //   deadline: "2020-03-01"
+  // });
+  // console.log(`Create new todo: ${newTodo.info} (ID: ${newTodo.id})`);
 
-let idCount = list.length
+  // Read all
+  const allTodoes = (await prisma.todoes()) || [];
+  console.log("All todoes", allTodoes);
+
+  // Delete a todo by id
+  const deleteTodo = await prisma.deleteTodo({
+    id: allTodoes[0] && allTodoes[0].id
+  });
+}
+
+main().catch(e => console.error(e));
+
+let list = [
+  {
+    id: "id-0",
+    info: "learn graphql",
+    deadline: "2020-01-05",
+    level: 3,
+    done: false,
+    createAt: "2020-01-03"
+  }
+];
+
+let idCount = list.length;
 
 const resolvers = {
   Query: {
@@ -24,24 +47,24 @@ const resolvers = {
         deadline: args.deadline,
         level: args.level || 0,
         done: false,
-        createAt: moment().format('YYYY-MM-DD')
-      }
-      list.push(todo)
-      return todo
+        createAt: moment().format("YYYY-MM-DD")
+      };
+      list.push(todo);
+      return todo;
     }
   },
   Todo: {
-    id: (root) => root.id,
-    info: (root) => root.info,
-    deadline: (root) => root.deadline,
-    level: (root) => root.level,
-    done: (root) => root.done,
-    createAt: (root) => root.createAt,
-  },
-}
+    id: root => root.id,
+    info: root => root.info,
+    deadline: root => root.deadline,
+    level: root => root.level,
+    done: root => root.done,
+    createAt: root => root.createAt
+  }
+};
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
